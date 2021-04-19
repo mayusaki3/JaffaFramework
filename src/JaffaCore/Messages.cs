8beec0e6d0db1e37bc8b6d16d5015df9d59f8b98
masayuki3 <masayuki@fa2.so-net.ne.jp>
2021-04-19 12:52:59 +0900

    少し修正。

--- src/JaffaCore/Messages.cs
+++ src/JaffaCore/Messages.cs
@@ -3,11 +3,17 @@
     public static class Messages
     {
         // Coreメッセージ --------------------------------------------------------------
-        // 起動時バージョン表示
+
+        /// <summary>
+        /// 起動時バージョン表示
+        /// </summary>
         public const string JFWI0001 = "JFWI0001 {JAFFA_TITLE} {JAFFA_VERSION} %0";
 
         // DateTimeメッセージ ----------------------------------------------------------
-        // 時刻変更
+
+        /// <summary>
+        /// 時刻変更
+        /// </summary>
         public const string JFWI0002 = "JFWI0002 {TIME_CHANGE} %0 => %1";
     }
 }
